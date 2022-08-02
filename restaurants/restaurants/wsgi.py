746246f1799342959936cd1d251a13b1716f2806
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-03 02:27:44 +0300

    test

--- restaurants/restaurants/wsgi.py
+++ restaurants/restaurants/wsgi.py
@@ -7,9 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+root_path = os.path.abspath(os.path.split(__file__)[0])
+sys.path.insert(0, os.path.join(root_path, 'restaurants'))
+sys.path.insert(0, root_path)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurants.settings')
 
 application = get_wsgi_application()
