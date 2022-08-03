e58cbbce4e00e3cf6c74d299b699893c9f863446
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-03 16:58:55 +0300

    test

--- restaurants/api/serializers.py
+++ restaurants/api/serializers.py
@@ -67,7 +67,7 @@ class ReviewSerializer(serializers.HyperlinkedModelSerializer):
 
     def create(self, validated_data):
         restaurant = validated_data.pop("restaurant")
-        restaurant_id = restaurant.keys()
+        restaurant_id = restaurant["id"].keys()
         raise ValidationError(detail=restaurant_id)
         try:
             restaurant = Restaurant.objects.get(id=restaurant_id)
