2b718ee81c34144a2da204e531e15bdbc1661147
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-03 14:22:20 +0300

    admin register fields updated

--- restaurants/main/admin.py
+++ restaurants/main/admin.py
@@ -13,22 +13,24 @@ class RestaurantAdmin(admin.ModelAdmin):
 
 @admin.register(Dish)
 class DishAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price')
-    fields = ('name', 'price', 'description', 'category', 'restaurant')
-    search_fields = ['name']
+    list_display = ('id', 'name_ru', 'name_en', 'price')
+    fields = ('name_ru', 'name_en', 'price', 'description', 'category', 'restaurant')
+    search_fields = ['name_ru', 'name_en']
 
 
 @admin.register(DishSet)
 class DishSetAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'image_tag')
-    fields = ('name', 'restaurant', 'dishes', 'image', 'image_tag')
+    list_display = ('id', 'name_ru', 'name_en', 'image_tag')
+    fields = ('name_ru', 'name_en', 'restaurant', 'dishes', 'image', 'image_tag')
     readonly_fields = ['image_tag']
+    search_fields = ['name_ru', 'name_en']
 
 
 @admin.register(DishesCategory)
 class DishesCategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    fields = ('restaurant', 'name', 'image')
+    list_display = ('id', 'name_ru', 'name_en')
+    fields = ('restaurant', 'name_ru', 'name_en', 'image')
+    search_fields = ['name_ru', 'name_en']
 
 
 @admin.register(RestaurantImage)
