6d631901a888faf3b43c2306ee1e0f816dfc351f
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-03 07:27:50 +0300

    restaurant images categories added

--- restaurants/main/models.py
+++ restaurants/main/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.html import mark_safe
+from django.conf import settings
 
 # Create your models here.
 
@@ -30,12 +32,30 @@ class DishSet(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     dishes = models.ManyToManyField(Dish)
     image = models.ImageField(upload_to="images/dish_sets/")
+
+    def image_tag(self):
+        return mark_safe(f'<img src="{self.image.url}" width="70" height="70" />')
+
+    image_tag.short_description = 'Image'
     
+
+class RestaurantImageCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
 
 class RestaurantImage(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    category = models.ForeignKey(RestaurantImageCategory, on_delete=models.CASCADE, null=True, blank=True)
     image = models.ImageField(upload_to="images/restaurants/")
     default = models.BooleanField(default=False)
+
+    def image_tag(self):
+        return mark_safe(f'<img src="{self.image.url}" width="70" height="70" />')
+
+    image_tag.short_description = 'Image'
+
 
