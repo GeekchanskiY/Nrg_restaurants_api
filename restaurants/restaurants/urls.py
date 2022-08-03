fdd3e911b8ae4cce17f5ff5cfc4270f309865c56
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-03 11:13:15 +0300

    important

--- restaurants/restaurants/urls.py
+++ restaurants/restaurants/urls.py
@@ -23,11 +23,11 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Restaurant API",
       default_version='v1',
-      description="Test description",
+      description="Гриша саси бибу я делать тык-тык",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
+      contact=openapi.Contact(email="dmt.webdevelopment@gmail.com"),
       license=openapi.License(name="BSD License"),
    ),
    public=True,
