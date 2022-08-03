1f9112f462856ab759b36785d5584a8a9c1fcf9c
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-03 11:02:07 +0300

    test view created

--- restaurants/api/views.py
+++ restaurants/api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from restaurants.main.models import Restaurant
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+from .serializers import RestaurantSerializer
 
 # Create your views here.
+
+
+@swagger_auto_schema(methods=["get", "post"], responses={200: RestaurantSerializer(many=True)})
+@api_view(['GET'])
+def test(request: Request):
+    if request.method == "GET":
+        restaurants = Restaurant.objects.all()
+        serializer = RestaurantSerializer(restaurants, many=True)
+        return serializer.data
+    elif request.method == "POST":
+        return Response(request.data, status=status.HTTP_200_OK)
