98b7e1356e445ab71fcee5793683521d81280362
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-03 17:00:37 +0300

    test

--- restaurants/api/serializers.py
+++ restaurants/api/serializers.py
@@ -67,8 +67,7 @@ class ReviewSerializer(serializers.HyperlinkedModelSerializer):
 
     def create(self, validated_data):
         restaurant = validated_data.pop("restaurant")
-        restaurant_id = restaurant["id"].keys()
-        raise ValidationError(detail=restaurant_id)
+        restaurant_id = restaurant["id"].id
         try:
             restaurant = Restaurant.objects.get(id=restaurant_id)
         except ObjectDoesNotExist:
