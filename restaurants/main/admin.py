4c1ed527813496c4499d5c0663559558ef7baef8
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-03 06:38:09 +0300

    restaurant admin created

--- restaurants/main/admin.py
+++ restaurants/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Restaurant
 
 # Register your models here.
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    fields = ('id', 'name')
+    search_fields = ['name']
+    
