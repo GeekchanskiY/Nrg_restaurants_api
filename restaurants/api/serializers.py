efea1d9aa1ba68d3367f49c38a40a97d5b1bf5f0
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-03 14:11:01 +0300

    serializers ready

--- restaurants/api/serializers.py
+++ restaurants/api/serializers.py
@@ -1,15 +1,31 @@
 from abc import ABC
 
 from rest_framework import serializers
-from main.models import Restaurant, Dish, DishesCategory
+from main.models import Restaurant, Dish, DishesCategory, DishSet, RestaurantImageCategory, RestaurantImage
+
+
+class RestaurantImageSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = RestaurantImage
+        fields = ('image', 'name')
+
+
+class RestaurantImageCategorySerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+    images_set = RestaurantImageSerializer(many=True)
+
+    class Meta:
+        model = RestaurantImageCategory
+        fields = ('id', 'name', 'images_set')
 
 
 class RestaurantSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.ReadOnlyField()
+    image_categories_set = RestaurantImageCategorySerializer(many=True)
 
     class Meta:
         model = Restaurant
-        fields = ('id', 'name')
+        fields = ('id', 'name', 'image_categories_set')
 
 
 class DishSerializer(serializers.HyperlinkedModelSerializer):
@@ -18,12 +34,22 @@ class DishSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Dish
-        fields = ('id', 'name', 'price', 'description', 'category_id')
+        fields = ('id', 'name_ru', 'name_en', 'price', 'description', 'category_id')
 
 
 class DishesCategorySerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.ReadOnlyField()
+    dishes_set = DishSerializer(many=True)
 
     class Meta:
         model = DishesCategory
-        fields = ('id', 'name', 'image')
+        fields = ('id', 'name_ru', 'name_en', 'image', 'dishes_set')
+
+
+class DishSetSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+    dishes_set = DishSerializer(many=True)
+
+    class Meta:
+        model = DishSet
+        fields = ('id', 'name_ru', 'name_en', 'image', 'dishes_set')
