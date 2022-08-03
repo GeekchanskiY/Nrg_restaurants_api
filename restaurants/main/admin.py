dace93f6018d06b906dfaa44ef8444e456f04aa9
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-03 07:28:23 +0300

    admin models registered

--- restaurants/main/admin.py
+++ restaurants/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Restaurant
+from .models import Restaurant, Dish, DishSet, DishesCategory, RestaurantImage, RestaurantImageCategory
 
 # Register your models here.
 
@@ -9,4 +9,37 @@ class RestaurantAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     fields = ['name']
     search_fields = ['name']
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price')
+    fields = ('name', 'price', 'description', 'category', 'restaurant')
+    search_fields = ['name']
+
+
+@admin.register(DishSet)
+class DishSetAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'image_tag')
+    fields = ('name', 'restaurant', 'dishes', 'image', 'image_tag')
+    readonly_fields = ['image_tag']
+
+
+@admin.register(DishesCategory)
+class DishesCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    fields = ('restaurant', 'name', 'image')
+
+
+@admin.register(RestaurantImage)
+class RestaurantImageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'image_tag')
+    fields = ('name', 'image', 'image_tag', 'restaurant', 'category', 'default')
+    readonly_fields = ['image_tag']
+
+
+@admin.register(RestaurantImageCategory)
+class RestaurantImageCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    fields = ('name', 'restaurant')
     
