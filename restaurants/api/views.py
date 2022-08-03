f47350f7a409ce309fcacd3d6ee930d15d1540a0
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-03 11:51:05 +0300

    test method

--- restaurants/api/views.py
+++ restaurants/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from main.models import Restaurant
+from main.models import Restaurant, Dish, DishesCategory
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -8,17 +8,26 @@ from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
-from .serializers import RestaurantSerializer
+from .serializers import RestaurantSerializer, DishSerializer, DishesCategorySerializer
+from django.db import connection, reset_queries
 
 # Create your views here.
 
 
-@swagger_auto_schema(methods=["get", "post"], responses={200: RestaurantSerializer(many=True)})
-@api_view(['GET', 'POST'])
-def test(request: Request):
-    if request.method == "GET":
-        restaurants = Restaurant.objects.all()
-        serializer = RestaurantSerializer(restaurants, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == "POST":
-        return Response(request.data, status=status.HTTP_200_OK)
+@swagger_auto_schema(method="get", responses={200: RestaurantSerializer(many=True)})
+@api_view('GET')
+def get_all_restaurants(request: Request):
+    restaurants = Restaurant.objects.all()
+    serializer = RestaurantSerializer(restaurants, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@swagger_auto_schema(method="get", responses={200: DishSerializer(many=True)})
+@api_view('GET')
+def get_all_restaurant_dishes(request: Request, pk: int):
+    reset_queries()
+    dishes = Dish.objects.filter(restaurant__id=pk)
+    
+    serializer = DishSerializer(dishes, many=True)
+    return Response({"queries": len(connection.queries), "dishes": serializer.data}, status=status.HTTP_200_OK)
+
