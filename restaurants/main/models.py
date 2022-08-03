ab03930a4c960e10c6bec90e4db1e76905c59a18
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-03 06:23:11 +0300

    models created

--- restaurants/main/models.py
+++ restaurants/main/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название", unique=True)
+
+
+class DishesCategory(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="images/dishes_categories/", null=True, blank=True)
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.TextField()
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    category = models.ForeignKey(DishesCategory, on_delete=models.CASCADE)
+
+
+class DishSet(models.Model):
+    name = models.CharField(max_length=255)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    dishes = models.ManyToManyField(Dish)
+    image = models.ImageField(upload_to="images/dish_sets/")
+    
+    
+
+class RestaurantImage(models.Model):
+    name = models.CharField(max_length=255)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="images/restaurants/")
+    default = models.BooleanField(default=False)
+
