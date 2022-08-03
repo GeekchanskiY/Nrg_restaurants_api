de170013738e7821cc200bbe61811595cf680727
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-04 02:39:23 +0300

    dish set view added

--- restaurants/api/views.py
+++ restaurants/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from main.models import Restaurant, Dish, DishesCategory, Review
+from main.models import Restaurant, Dish, DishesCategory, Review, DishSet
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -9,7 +9,9 @@ from rest_framework.exceptions import ValidationError
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
-from .serializers import RestaurantSerializer, DishSerializer, DishesCategorySerializer, ReviewSerializer
+from .serializers import RestaurantSerializer, DishSerializer, DishesCategorySerializer, ReviewSerializer,\
+    DishSetSerializer
+
 from django.db import connection, reset_queries
 
 # Create your views here.
@@ -70,5 +72,13 @@ def post_restaurant_review(request: Request):
 def get_restaurant_reviews(request: Request, pk: int):
     reviews = Review.objects.filter(restaurant__id=pk)
     serializer = ReviewSerializer(reviews, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@swagger_auto_schema(method="get", responses={200: DishSetSerializer(many=True)})
+@api_view(['GET'])
+def get_restaurant_dish_sets(request: Request, pk: int):
+    dish_sets = DishSet.objects.filter(restaurant__id=pk)
+    serializer = DishSetSerializer(dish_sets, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
