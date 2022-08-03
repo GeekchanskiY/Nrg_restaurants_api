86c0f99e9a4844b032953bd2f9330fd34d175c05
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-03 06:43:30 +0300

    id added

--- restaurants/main/models.py
+++ restaurants/main/models.py
@@ -4,16 +4,19 @@ from django.db import models
 
 
 class Restaurant(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, verbose_name="Название", unique=True)
 
 
 class DishesCategory(models.Model):
+    id = models.AutoField(primary_key=True)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     image = models.ImageField(upload_to="images/dishes_categories/", null=True, blank=True)
 
 
 class Dish(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     price = models.FloatField()
     description = models.TextField()
@@ -22,14 +25,15 @@ class Dish(models.Model):
 
 
 class DishSet(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     dishes = models.ManyToManyField(Dish)
     image = models.ImageField(upload_to="images/dish_sets/")
     
-    
 
 class RestaurantImage(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="images/restaurants/")
