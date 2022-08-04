c634c3fdea6e54343c006204bc1187cfe74cffcb
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-05 01:17:55 +0300

    register view added

--- restaurants/main/views.py
+++ restaurants/main/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import  render, redirect, HttpResponse
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
 
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful." )
+            return HttpResponse("Registered")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render(request=request, template_name="register.html", context={"register_form": form})
+
