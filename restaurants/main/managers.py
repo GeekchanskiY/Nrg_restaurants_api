1a3ef1893d6473df8e27130dd4d61c41032d66f3
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-04 21:10:41 +0300

    user manager created

---
+++ restaurants/main/managers.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    User model manager
+    """
+    def create_user(self, username, password, **extra_fields):
+        """
+        Create and save a User
+        """
+        if not username:
+            raise ValueError('The Email must be set')
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
