5a1edcb4a3e3db5170b6d5e5789cfccdc72157c6
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-05 01:17:47 +0300

    register form created

---
+++ restaurants/main/forms.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import AdminUser
+from django import forms
+
+
+# Create your forms here.
+
+class NewUserForm(UserCreationForm):
+	username = forms.CharField(max_length=255, required=True)
+
+	class Meta:
+		model = AdminUser
+		fields = ("username", "password1", "password2")
+
+	def save(self, commit=True):
+		user = super(NewUserForm, self).save(commit=False)
+		user.username = self.cleaned_data['username']
+		if commit:
+			user.save()
+		return user
