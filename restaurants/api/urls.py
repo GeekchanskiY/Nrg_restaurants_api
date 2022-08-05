76d8200e6bc7713bbd19fd2d59d3f59b60b1dec9
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-05 22:12:32 +0300

    endpoints changed

--- restaurants/api/urls.py
+++ restaurants/api/urls.py
@@ -15,14 +15,12 @@ Including another URLconf
 """
 
 from django.urls import path
-from .views import get_all_restaurants, get_all_restaurant_dishes, get_dishes_category, post_restaurant_review,\
-    get_restaurant_reviews, get_restaurant_dish_sets, get_all_news, get_all_news_exclude
+from .views import get_all_restaurants, post_restaurant_review,\
+    get_restaurant_reviews, get_all_news, get_all_news_exclude, get_all_dish_restaurant_data
 
 urlpatterns = [
     path('restaurants/all', get_all_restaurants, name="get_all_restaurants"),
-    path('dishes/restaurant/<int:pk>', get_all_restaurant_dishes, name="get_all_restaurant dishes"),
-    path('dishes/category/<int:pk>', get_dishes_category, name="get_dishes_category"),
-    path('dishes/dish_sets/<int:pk>', get_restaurant_dish_sets, name="get_dish_sets"),
+    path('restaurant/<int:pk>/dishes', get_all_dish_restaurant_data, name="get_all_restaurant dishes"),
     path('reviews/create', post_restaurant_review, name="create_restaurant_review"),
     path('reviews/get/<int:pk>', get_restaurant_reviews, name="get_restaurant_reviews"),
     path('news/all', get_all_news, name="get_all_news"),
