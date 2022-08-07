173751e6d05d6c76e2e7bd3d08b38869c7e983a0
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-07 20:38:29 +0300

    get news by id endpoint added

--- restaurants/api/urls.py
+++ restaurants/api/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from django.urls import path
 from .views import get_all_restaurants, post_restaurant_review,\
-    get_restaurant_reviews, get_all_news, get_all_news_exclude, get_all_dish_restaurant_data
+    get_restaurant_reviews, get_all_news, get_all_news_exclude, get_all_dish_restaurant_data, get_news_by_id
 
 urlpatterns = [
     path('restaurant/all', get_all_restaurants, name="get_all_restaurants"),
@@ -24,5 +24,6 @@ urlpatterns = [
     path('reviews/create', post_restaurant_review, name="create_restaurant_review"),
     path('reviews/get/<int:pk>', get_restaurant_reviews, name="get_restaurant_reviews"),
     path('news/all', get_all_news, name="get_all_news"),
-    path('news/exclude/<int:pk>', get_all_news_exclude, name="get_all_news_exclude")
+    path('news/exclude/<int:pk>', get_all_news_exclude, name="get_all_news_exclude"),
+    path('news/get/<int:pk>', get_news_by_id, name="get_news_by_id")
 ]
