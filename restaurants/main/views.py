eaa61ecf85c38ff5b3ddbee281fadb46b0bd5119
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-08 06:00:41 +0300

    import/export init

--- restaurants/main/views.py
+++ restaurants/main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import  render, redirect, HttpResponse
-from .forms import NewUserForm
+from .forms import NewUserForm, UploadDataForm
+from .models import Restaurant, Dish
 from django.contrib.auth import login
 from django.contrib import messages
+import io
+import xlsxwriter
 
 
 def register(request):
@@ -16,3 +19,33 @@ def register(request):
     form = NewUserForm()
     return render(request=request, template_name="register.html", context={"register_form": form})
 
+
+def handle_uploaded_file(file):
+    with open('data.xlsx', 'wb+') as f:
+        for chunk in file.chunks():
+            f.write(chunk)
+
+
+def import_view(request):
+    if request.method == "POST":
+        form = UploadDataForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponse("Success")
+    else:
+        form = UploadDataForm()
+
+    return render(request, "upload_file.html", {"upload_file_form": form})
+
+
+def export_view(request):
+    if request.user.restaurant is None:
+        return HttpResponse("Вы должны быть авторизованы с аккаунта владельца ресторана")
+
+    dishes = Dish.objects.filter(id=request.user.restaurant.id)
+
+    output = io.BytesIO()
+    workbook = xlsxwriter.Workbook(output)
+    worksheet = workbook.add_worksheet()
+
+    return HttpResponse("test")
