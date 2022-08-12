6e20bf26f1ad3ee809825bb6e1c72d55e4b7eab6
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-12 06:48:24 +0300

    test

--- restaurants/main/views.py
+++ restaurants/main/views.py
@@ -21,7 +21,7 @@ def register(request):
     return render(request=request, template_name="register.html", context={"register_form": form})
 
 
-def handle_uploaded_file(file):
+def save_uploaded_file(file):
     with open('data.xlsx', 'wb+') as f:
         for chunk in file.chunks():
             f.write(chunk)
@@ -31,7 +31,8 @@ def import_view(request):
     if request.method == "POST":
         form = UploadDataForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request.FILES['file'])
+            save_uploaded_file(request.FILES['file'])
+            workbook = xlsxwriter.Workbook()
             return HttpResponse("Success")
     else:
         form = UploadDataForm()
@@ -53,17 +54,18 @@ def export_view(request):
     dish_worksheet.write(0, 2, "цена")
     dish_worksheet.write(0, 3, "Описание рус")
     dish_worksheet.write(0, 4, "Описание англ")
-    dish_worksheet.write(0, 5, "Название категории на русском (опционально, категория должна существовать в бд)")
+    dish_worksheet.write(0, 5, "Названия категорий на русском через ; (опционально)")
+    dish_worksheet.write(0, 6, "Названия наборов на русском через ; (опционально)")
 
     for row_num, dish in enumerate(dishes):
-        dish_worksheet.write(2 + row_num, 1, dish.name_ru)
-        dish_worksheet.write(2 + row_num, 2, dish.name_en)
-        dish_worksheet.write(2 + row_num, 3, dish.price)
-        dish_worksheet.write(2 + row_num, 4, dish.description_ru)
-        dish_worksheet.write(2 + row_num, 5, dish.description_en)
-        category = dish.dishescategory_set.first()
-        if category is not None:
-            dish_worksheet.write(2 + row_num, 6, category.name_ru)
+        dish_worksheet.write(1 + row_num, 1, dish.name_ru)
+        dish_worksheet.write(1 + row_num, 2, dish.name_en)
+        dish_worksheet.write(1 + row_num, 3, dish.price)
+        dish_worksheet.write(1 + row_num, 4, dish.description_ru)
+        dish_worksheet.write(1 + row_num, 5, dish.description_en)
+        categories = dish.dishescategory_set.all().only("name_ru")
+        if categories is not None and len(categories) != 0:
+            dish_worksheet.write(1 + row_num, 6, ";".join(categories))
     workbook.close()
 
     return FileResponse(open("output.xlsx", "rb"))
