ff615d38e4643328bcf0585248c3d5da6a3a9857
GeekchanskiY <astrovskiy.04@gmail.com>
2022-08-12 07:03:48 +0300

    import view added

--- restaurants/main/urls.py
+++ restaurants/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import register, export_view
+from .views import register, export_view, import_view
 
 urlpatterns = [
     path('register', register, name="register"),
-    path('export', export_view, name="export")
+    path('export', export_view, name="export"),
+    path('import', import_view, name="import")
 ]
